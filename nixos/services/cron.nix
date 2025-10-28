2cbae221ba5d5a23f12fa00a275fc98320e7857b
Ben Feather <contact@benfeather.dev>
2025-10-28 14:13:46 +1300

    WIP

--- nixos/services/cron.nix
+++ nixos/services/cron.nix
@@ -6,8 +6,8 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "0 1 * * *    root    backup-appdata"
-      "0 2 * * *    root    oci-containers upgrade"
+      "0 0 1-31/3 * *     root    backup-appdata"
+      "0 1 * * *          root    oci-containers upgrade"
     ];
   };
 }
