2de1d25727ec19ea49351d2f1d3bd431a44b6b31
Ben Feather <contact@benfeather.dev>
2026-03-08 12:47:50 +1300

    Fix quoting in fix-permissions script for directory and file paths

--- nixos/scripts/fix-permissions.nix
+++ nixos/scripts/fix-permissions.nix
@@ -12,15 +12,15 @@ let
     fi
 
     echo "Fixing permissions in ${env.root_dir}..."
-    chown -R ${env.puid}:users ${env.root_dir}
-    find ${env.root_dir} -type d -exec chmod 750 {} \;
-    find ${env.root_dir} -type f -exec chmod 640 {} \;
+    chown -R ${env.puid}:users "${env.root_dir}"
+    find "${env.root_dir}" -type d -exec chmod 750 {} \;
+    find "${env.root_dir}" -type f -exec chmod 640 {} \;
     echo "Permissions fixed."
 
     echo "Fixing permissions in ${env.appdata_dir}..."
-    chown -R ${env.puid}:docker ${env.appdata_dir}
-    find ${env.appdata_dir} -type d -exec chmod 777 {} \;
-    find ${env.appdata_dir} -type f -exec chmod 666 {} \;
+    chown -R ${env.puid}:docker "${env.appdata_dir}"
+    find "${env.appdata_dir}" -type d -exec chmod 777 {} \;
+    find "${env.appdata_dir}" -type f -exec chmod 666 {} \;
     echo "Permissions fixed."
   '';
 in
